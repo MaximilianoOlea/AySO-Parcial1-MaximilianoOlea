f5c9d765909103d7c59001b9616d379eec99c049
MaximilianoOlea <aomaxi946@gmail.com>
2024-10-13 23:14:38 +0000

    terminado c

---
+++ puntoc.sh
@@ -0,0 +1,7 @@
+sudo usermod -aG grupodevelopers developer1
+sudo usermod -aG grupodevops developer1
+sudo usermod -aG grupotesters tester1
+sudo usermod -aG grupodevops devops1
+sudo usermod -aG grupodevelopers devops1
+sudo usermod -aG grupodevops devops2
+
