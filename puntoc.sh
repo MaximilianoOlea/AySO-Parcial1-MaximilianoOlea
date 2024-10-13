2dc4c76e183feefa0dcc439c9346bf994d98a4af
MaximilianoOlea <aomaxi946@gmail.com>
2024-10-13 23:38:58 +0000

    terminado el c

--- puntoc.sh
+++ puntoc.sh
@@ -10,3 +10,16 @@ sudo usermod -aG grupodevops devops1
 sudo usermod -aG grupodevelopers devops1
 sudo usermod -aG grupodevops devops2
 
+sudo chmod -R 750 /Examenes-UTN/alumno_1
+sudo chmod -R 750 Examenes-UTN/alumnos_1
+sudo chmod -R 760 Examenes-UTN/alumnos_2
+sudo chmod -R 700 Examenes-UTN/alumnos_3
+sudo chmod -R 775 Examenes-UTN/profesores
+
+whoami > Examenes-UTN/alumnos_1/validar.txt
+ll Examenes-UTN/alumnos_1/validar.txt
+cat Examenes-UTN/alumnos_1/validar.txt
+whoami > Examenes-UTN/alumnos_2/validar.txt
+whoami > Examenes-UTN/alumnos_3/validar.txt
+whoami > Examenes-UTN/profesores/validar.txt
+
