acf88d0ec309011b38d28dbbb433b1f5213e31cf
MaximilianoOlea <aomaxi946@gmail.com>
2024-10-13 23:45:39 +0000

    terminado e

---
+++ puntoe.sh
@@ -0,0 +1,5 @@
+cat /proc/meminfo | grep MemTotal
+cat /proc/meminfo | grep MemTotal > Filtro_Basico.txt
+sudo dmidecode -t chassis |grep Manufacturer
+sudo dmidecode -t chassis |grep Manufacturer >> Filtro_Basico.txt
+
